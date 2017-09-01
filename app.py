0519244baab770686cbcd9de7a047e40104acdc9
sjang <sjang3@students.towson.edu>
2017-09-01 17:43:05 -0400

    add1

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
+    log("hi")
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
     if data["object"] == "page":
